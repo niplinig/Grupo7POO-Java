4342c6b6b8c00c179816f4c0c0b9975187f8a7f5
Nicolás Andrés Plaza Iñiguez <74793371+niplinig@users.noreply.github.com>
2021-07-07 17:26:08 +0000

    Cambio de las variables de listas a datos primitivos

--- scr/Sensor.java
+++ scr/Sensor.java
@@ -2,24 +2,16 @@ import java.util.ArrayList;
 
 public class Sensor{
 
-	String idDispositivo;
-	ArrayList <float> listaCo = new ArrayList<float>();
-	ArrayList <float> listaHuminity = new ArrayList<float>();
-	ArrayList <boolean> listaLight = new ArrayList<>();
-	ArrayList <float> listaLpg = new ArrayList<>();
-	ArrayList <boolean> listaMotion = new ArrayList<>();
-	ArrayList <float> listaSmoke = new ArrayList<>();
-	ArrayList <float> listaTemp = new ArrayList<>();
-	ArrayList <Fecha> listaFechas = new ArrayList<>();
+	private String idDispositivo;
+	float co;
+	float huminity;
+	boolean light;
+	float lpg;
+	boolean motion;
+	float smoke;
+	float temp;
+	String fecha;
 
-	public Sensor(String idDispositivo, float co, float huminity, boolean light, float lpg, boolean motion, float smoke, float temp, Fecha fecha) {
-		this.idDispositivo = idDispositivo;
-		listaCo.add(co);
-		listaHuminity.add(huminity);
-		listaLight.add(light);
-		listaLpg.add(lpg);
-		listaMotion.add(motion);
-		listaSmoke.add(smoke);
-		listaTemp.add(temp);
+	public Sensor(String idDispositivo, float co, float huminity, boolean light, float lpg, boolean motion)
 
 }
