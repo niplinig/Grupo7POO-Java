288a4d4c658a0e483fc128f5a4b62a187b02e958
Nicolás Andrés Plaza Iñiguez <74793371+niplinig@users.noreply.github.com>
2021-07-07 17:28:37 +0000

    Nuevo constructor

--- scr/Sensor.java
+++ scr/Sensor.java
@@ -1,17 +1,27 @@
 import java.util.ArrayList;
 
 public class Sensor{
+//Atributos de sensores
+//Un sensor puede aparecer mas de una vez, pero son objetos diferentes porque comparten ID pero no los demás atributos
+	private String ID;
+	private float co;
+	private float huminity;
+	private boolean light;
+	private float lpg;
+	private boolean motion;
+	private float smoke;
+	private float temp;
+	private String fecha;
 
-	private String idDispositivo;
-	float co;
-	float huminity;
-	boolean light;
-	float lpg;
-	boolean motion;
-	float smoke;
-	float temp;
-	String fecha;
-
-	public Sensor(String idDispositivo, float co, float huminity, boolean light, float lpg, boolean motion)
+	public Sensor(String ID, float co, float huminity, boolean light, float lpg, boolean motion, float smoke, float temp, String fecha){
+    this.ID=ID;
+		this.co = co;
+		this.huminity = huminity;
+		this.light = light;
+		this.lpg = lpg;
+		this.motion = motion;
+		this.smoke = smoke;
+		this.temp = co;
+  }
 
 }
