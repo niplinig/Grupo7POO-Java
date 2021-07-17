05b8ef0fefc2a310db9a55c4ae7c76890c5cee80
HwrdL <74841258+HwrdL@users.noreply.github.com>
2021-07-17 14:32:13 -0500

    Create creadorArchivo.java

---
+++ Notificacion/creadorArchivo.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
+
+public class CreadroArchivo {
+    public String texto;
+    public String nombreArch;
+    
+
+    
+    public static void guardarArchivo(String texto,String nombreArch){
+        try
+        {
+            FileWriter fw = new FileWriter(nombreArch,true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            
+            pw.println(texto);
+            pw.flush();
+            pw.close();
+            
+            JOptionPane.showMessageDialog(null,"Archivo guardado");
+        }
+        catch(Exception E)
+        {
+            JOptionPane.showMessageDialog(null,"Archivo no guardado");
+            
+        }
+    }
