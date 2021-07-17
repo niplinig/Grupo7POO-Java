1172f1a258bb839d86c0b3c103192eb1961a59fd
Nicolás Andrés Plaza Iñiguez <74793371+niplinig@users.noreply.github.com>
2021-07-17 18:35:30 -0500

    Create Notificacion.java

---
+++ Notificacion/Notificacion.java
@@ -0,0 +1,11 @@
+package main;
+
+
+public class Notificacion {
+    private Boolean estatus = true;
+    private String nombre;
+    
+    public void desactivarNotificacion() {
+        estatus = false;
+    }
+}
