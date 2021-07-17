975ba3322c7aaf0861b92eb3d104b7d636062451
Nicolás Andrés Plaza Iñiguez <74793371+niplinig@users.noreply.github.com>
2021-07-17 18:34:53 -0500

    Update Libreria.java

--- LecturaArchivo/Libreria.java
+++ LecturaArchivo/Libreria.java
@@ -7,26 +7,9 @@ public class Libreria {
     // La libreria tiene la lista de todos los dispositivos disponibles
     public static ArrayList <Dispositivo> listaDispositivos = new ArrayList<>();
     public static ArrayList <String> listaCodigoDispositivos = new ArrayList<>();
-    
-    /*
-    Método que recibe un dispositivo
-    Compara el dispositivo recibido con los dispositivos en la cadena de dispositivos
-    Verifica si el dispositivo puede ser añadido a la lista
-    Si el dispositivo no puede ser añadido significa que ya existe
-    Por lo tanto actualiza los datos del dispositivo que se encuentra en lista
-    */
-    public static void addDevice(Dispositivo nuevoDispositivo) {
-        
-        if (nuevoDispositivo.puedeSerAnadido()) {
-            listaDispositivos.add(nuevoDispositivo);
-        }
-        else {
-            for (Dispositivo dispositivoEnLista : listaDispositivos) {
-                if (nuevoDispositivo.equals(dispositivoEnLista)) {
-                    dispositivoEnLista.listaObservaciones.addAll(nuevoDispositivo.listaObservaciones);
-                }
-            }
-        }
+
+    static void listaDispositivos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
