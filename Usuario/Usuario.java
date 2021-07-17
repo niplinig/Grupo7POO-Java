348c4ef03425ba29b46cbeb5c552bdc320a343f7
GaboHep <86680331+GaboHep@users.noreply.github.com>
2021-07-17 16:29:49 -0500

    Update Usuario.java
    Actualización de clase Usuario a partir del nuevo método de lectura de archivos

--- Usuario/Usuario.java
+++ Usuario/Usuario.java
@@ -1,42 +1,36 @@
-package Usuario;
+package main;
 
-import Sensores.Sensor;
-import Notificaciones.Notificacion;
 import java.util.ArrayList;
 
-
 public class Usuario {
-    //Atributos de la clase Usuario
     private String id;
-    //La clase usuario es más de almacenamiento de los datos asociados con un usuario
-    private ArrayList<String> dispAsociados=new ArrayList<>(); //Aqui talvez haya que cambiar por que cuando es
-    //notificacion por dispositivo asociado solo es para los dispositivos asociados.
-    private ArrayList<Notificacion> notificacionesUsuario=new ArrayList<>();
+    private ArrayList <Dispositivo> dispAsociados=new ArrayList<>();
+    private ArrayList <Notificacion> nProgramadas=new ArrayList<>();
     
-    //Constructor (Se utiliza al registrar un usuario)
+    /*Constructor, solo preciso del id para crear un usuario (A se llamado en
+    Registrarse()
+    */
     public Usuario(String id){
         this.id=id;
     }
-    
-    //Getter ID Utilizado para el inicio de sesión
-    public String getID(){
+    //Getters de los atributos de la clase Usuario
+     public String getID(){
         return id;
     }
-    
-    //Métodos para añadir datos a las listas
-    public void vincularDispositivo(String id){
-        dispAsociados.add(id);
-    }
-    
-    public void añadirNotificacion(Notificacion n){
-        notificacionesUsuario.add(n);
-    }
-    
-    public ArrayList getListDisp(){
+     public ArrayList getListDisp(){
         return dispAsociados;
     }
     public ArrayList getListNotis(){
-        return notificacionesUsuario;
+        return nProgramadas;
+    }
+    
+    //Métodos para añadir elementos a las listas objetos
+    
+    public void vincularDispositivo(Dispositivo d){
+        dispAsociados.add(d);
+    }
+    public void añadirNoti(Notificacion n){
+        nProgramadas.add(n);
     }
     
 }
