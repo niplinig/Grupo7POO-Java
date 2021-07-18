69512e157f9a4f3b5cf266685d1bf75be2680e3b
Nicolás Andrés Plaza Iñiguez <74793371+niplinig@users.noreply.github.com>
2021-07-17 19:16:49 -0500

    Update NotificacionDispositivo.java

--- Notificacion/NotificacionDispositivo.java
+++ Notificacion/NotificacionDispositivo.java
@@ -1,117 +1,34 @@
 package main;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class NotificacionPropiedadObservable extends Notificacion {
-    // La etiqueta nos dice el grado de importancia de la notificacion
-    private String etiqueta;
-    // La propiedad que el usuario selecciona de las multiples observaciones
-    private String propiedad;
+public class NotificacionDispositivo extends Notificacion{
+
+    private LocalDate fechaInicial;
+    private LocalDate fechaFinal;
+
+        
     
-    private boolean propiedadBooleana;
-    
-    // El valor máximo
-    private int maximo = 0;
-    // El valor mínimo
-    private int minimo = 0;
-    
-    // Constructor completo, inicializa los atributos del rango
-    public NotificacionPropiedadObservable(String nombre, int min, String etiqueta, String propetie, int max) {
+    public NotificacionDispositivo(String nombre, LocalDate fechaInicial, LocalDate fechaFinal) {
         super(nombre);
-        this.etiqueta = etiqueta;
-        this.propiedad = propetie;
-        this.maximo = max;
-        this.minimo = min;
+        this.fechaInicial = fechaInicial;
+        this.fechaFinal = fechaFinal;
     }
-    
-    // Constructor con el mínimo valor, inicializa solo él mínimo valor
-    // Para realizar búsquedas con valores mayores al mínimo
-    public NotificacionPropiedadObservable(String nombre, int min, String etiqueta, String propetie) {
-        super(nombre);
-        this.etiqueta = etiqueta;
-        this.propiedad = propetie;
-        this.minimo = min;
-    }
-    
-    // Constructor con el máximo valor, inicializa solo el máximo valor
-    // Para realizar búsquedas con valores menores al máximo
-    public NotificacionPropiedadObservable(String nombre, String etiqueta, String propetie, int max) {
-        super(nombre);
-        this.etiqueta = etiqueta;
-        this.propiedad = propetie;
-        this.maximo = max;
-    }
-    
-    public NotificacionPropiedadObservable(String nombre, String etiqueta, String propiedad, String valorBoleano) {
-        super(nombre);
-        this.etiqueta = etiqueta;
-        this.propiedad = propiedad;
-        this.propiedadBooleana = Boolean.parseBoolean(valorBoleano);
-    }
-    
-    public ArrayList<Observacion> aplicarNPO(Usuario usuario) {
+
+    public ArrayList<Observacion> tomarUnRango(Usuario usuario, Dispositivo dispositivo) {
         
         ArrayList <Observacion> listaResultante = new ArrayList<>();
         
-        String propiedaElegida = this.elegirPropiedad();
-        
         if (!usuario.getListDisp().isEmpty()) {
-            
-            if (propiedaElegida.equals("double") ) {
-                
-                for (Dispositivo dispositivos : usuario.getListDisp()) {
-                    
-                    for (Observacion observacion: dispositivos.listaObservaciones) {
-                        
-                        if (minimo > 0 && maximo > 0) {
-                            if(minimo < observacion.getConjunto().getPropiedadDouble(propiedad) && observacion.getConjunto().getPropiedadDouble(propiedad) < maximo) {
-                                listaResultante.add(observacion);
-                            }
-                        }
-                        
-                        else if (minimo > 0 && maximo == 0) {
-                            if (minimo < observacion.getConjunto().getPropiedadDouble(propiedad)) {
-                                listaResultante.add(observacion);
-                            }
-                        }
-                        
-                        else if (minimo == 0 && maximo > 0) {
-                            if (observacion.getConjunto().getPropiedadDouble(propiedad) < maximo) {
-                                listaResultante.add(observacion);
-                            }
-                        }
-                    }
-                }
-            }
-            
-            else if (propiedaElegida.equals("boolean")) {
-                
-                for (Dispositivo dispositivos : usuario.getListDisp()) {
-
-                    for (Observacion observacion: dispositivos.listaObservaciones) {
-
-                        if (observacion.getConjunto().getPropiedadBoolean(propiedad) == propiedadBooleana) {
-
-                            listaResultante.add(observacion);
-                        }
+            for (Dispositivo dispositivos : usuario.getListDisp()) {
+                for (Observacion observacion: dispositivos.listaObservaciones) {
+                    if (observacion.getFecha().isAfter(fechaInicial) && observacion.getFecha().isBefore(fechaFinal)) {
+                        listaResultante.add(observacion);
                     }
                 }
             }
         }        
         return listaResultante;
-    }
-    
-    public String elegirPropiedad() {  
-        
-        String propiedadElegida;
-        
-        if (propiedad.equals("co") || propiedad.equals("humidity") || propiedad.equals("lpg") || propiedad.equals("smoke") || propiedad.equals("temp")) {
-            return propiedadElegida = "double";
-        }
-        
-        else if (propiedad.equals("light") || propiedad.equals("motion")) {
-            return propiedadElegida = "boolean";
-        }
-        return propiedadElegida = "";
     }
 }
