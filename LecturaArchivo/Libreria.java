b96f4e41cb7499c6a82f9691a30477e02aecef20
HwrdL <74841258+HwrdL@users.noreply.github.com>
2021-07-17 23:56:32 -0500

    Update Libreria.java

--- LecturaArchivo/Libreria.java
+++ LecturaArchivo/Libreria.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Libreria {
     
-    // La libreria tiene la lista de todos los dispositivos disponibles
+    // La libreria tiene la lista de todos los dispositivos disponibles.
     public static ArrayList <Dispositivo> listaDispositivos = new ArrayList<>();
     public static ArrayList <String> listaCodigoDispositivos = new ArrayList<>();
     
